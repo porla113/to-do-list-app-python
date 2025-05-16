e5f17c504f5b7cf3aa88b4e4f58178ea5bb86ce2
Por Cho <porla113@gmail.com>
2025-05-16 18:10:23 +0700

    update how to select operation, add menu, and use with for file object handling

--- main.py
+++ main.py
@@ -1,46 +1,62 @@
+# Greeting message
 print("\nHi! Welcome to Todo List App\n")
 
-file = open('todos.txt', 'r')
-todos = file.readlines()
-file.close()
+# Create todos list from a text file
+with open('todos.txt', 'r') as file:
+    todos = file.readlines()
+
+# Menu
+optMenu = """
+========== Operation ===========
+1. Add          4. Complete
+2. Show         5. Exit
+3. Edit
+"""
 
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.strip()
+    print(optMenu)
+    user_action = int(input("Select an operation with number 1-5: "))
+    print("\n")
 
     match user_action:
-        case 'add':
+        case 1:
+            # Add operation
             todo = input("Enter a todo: ") + "\n"
             
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
-        case 'show':
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        case 2:
+            # Show operation
+            print("========== To-Do List ==========")
             toShow = [todo.strip("\n") for todo in todos]
 
             for index, item in enumerate(toShow):
                 row = f"{index + 1}-{item}"
                 print(row)
-        case 'edit':
+        case 3:
+            # Edit operation
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter a todo: ") + "\n"
             todos[number] = new_todo
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
-        case 'complete':
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        case 4:
+            # Complete operation
             number = int(input("Number of the todo to complete: "))
             number = number - 1
             todos.pop(number)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
-        case 'exit':
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        case 5:
+            # Exit operation
             break
+        case _:
+            print("Please input 1-5.")
 
 print("\nGoodbye!\n")
