c537597408faaf5c2b823e724e8f920ab5c42d92
Por Cho <porla113@gmail.com>
2025-05-18 09:07:38 +0700

    create custom functions get_todos(), set_todos()

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Greeting message
-print("\nHi! Welcome to Todo List App\n")
-
-# Create todos list from a text file
-with open('todos.txt', 'r') as file:
-    todos = file.readlines()
-
+# Variables
+todos_file = 'todos.txt'
 # Menu
 optMenu = """
 ========== Operation ===========
@@ -12,7 +7,39 @@ optMenu = """
 2. Show         5. Exit
 3. Edit
 """
+# Functions
+# get_todos function
+def get_todos(filepath):
+    """
+    Open and read from a file
+    Arguments:
+    - filepath - string
+    Return:
+    - a list of string
+    """
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
 
+# set_todos function
+def set_todos(filepath, todos_list):
+    """
+    Write a string list to a file
+    Arguments:
+    - filepath: string
+    - todos_list: a list of string
+    Return:
+    - None
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_list)
+
+# Greeting message
+print("\nHi! Welcome to Todo List App\n")
+
+# Create todos list from a text file
+todos = get_todos(todos_file)
+print(type(todos))
 while True:
     print(optMenu)
     user_action = int(input("Select an operation with number 1-5: "))
@@ -22,11 +49,9 @@ while True:
         case 1:
             # Add operation
             todo = input("Enter a todo: ") + "\n"
-            
             todos.append(todo)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            set_todos(todos_file, todos)
 
         case 2:
             # Show operation
@@ -43,16 +68,14 @@ while True:
             new_todo = input("Enter a todo: ") + "\n"
             todos[number] = new_todo
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            set_todos(todos_file, todos)
         case 4:
             # Complete operation
             number = int(input("Number of the todo to complete: "))
             number = number - 1
             todos.pop(number)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            set_todos(todos_file, todos)
         case 5:
             # Exit operation
             break
